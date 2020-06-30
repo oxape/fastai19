95769fda8af8a5a2242934be4320deb3d9446bc4
donglu <donglu@starot.com>
2020-06-30 20:54:20 +0800

    add learning.py

---
+++ learning.py
@@ -0,0 +1,9 @@
+from fastai.vision import *
+from fastai.text import *
+from fastai.collab import *
+from torchvision import datasets
+
+if __name__ == '__main__':
+    p = Path('../data/mnist')
+    p.ls()
+    print('fastai')
