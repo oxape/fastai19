3f74a6a3877e1a91dfd9b57867a21c9396f5661c
donglu <donglu@starot.com>
2020-07-01 20:29:27 +0800

    modify learning

--- api-learning/learning.py
+++ api-learning/learning.py
@@ -1,9 +1,18 @@
 from fastai.vision import *
-from fastai.text import *
 from fastai.collab import *
-from torchvision import datasets
+from fastai.metrics import error_rate
 
 if __name__ == '__main__':
+    p = Path()
+    print(p.resolve())
     p = Path('../data/mnist')
-    p.ls()
-    print('fastai')
+    tfms = get_transforms(do_flip=False)
+    data = (ImageList.from_folder(p)
+            .split_by_folder(train='train', valid='valid')
+            .label_from_folder()
+            .transform(tfms)
+            .databunch(bs=16, num_workers=0))
+    data.show_batch(3, figsize=(6, 6), hide_axis=False)
+    learner = cnn_learner(data, models.resnet18, metrics=error_rate)
+    learner.lr_find()
+    learner.recorder.plot()
